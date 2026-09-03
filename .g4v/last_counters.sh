ae52172a7e2bc2d4bf9d427fc62882dab2f336fb
Victor N. Skurikhin <vs@lnhost.net>
2026-09-03 13:15:29 +0300

    comment for InstallSnapshot and README.md

--- .g4v/last_counters.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-# last_counters.sh <leader-node>
-# Печатает последнюю строку счётчиков из stdout узла-лидера.
-# Строка счётчиков содержит "VerifyDone=" (уникальный маркер countersReport).
-set -u
-N="$1"
-F="trace/raftkv-$N.stdout"
-grep 'VerifyDone=' "$F" 2>/dev/null | tail -n 1
